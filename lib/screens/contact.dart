eed90e65d7f6f96c82e43ee3bcdc84d3c1bd0118
Chad <chad__thomas@hotmail.com>
2023-05-31 12:54:40 -0600

    Chadlyalan/issue13 (#15)
    * Icons on About Cards
    
    I didn't like how the text was looking on the about cards so I made the decision to use Icons on the cards, with a bigger title to display the things I'm "about."
    I created a custom SVG for Project Management, found a flutter SVG, and modified the color of the "onHover" so that there is some opacity as well.
    I LOVE how it's turned out so far. I still have to finish the about cards but this is a good step to making the site prettier as well as functional.
    
    * about me cleaning
    
    tidying up the about me section a bit more by adding some more logo/icons for the sections. Now they're a bit more colorful. I have added a couple other's that I plan to use in other places on the website, like the github logo.
    
    * project size + hyperlink
    
    The project's page size is now dynamic to how many cards there are rather than a single page's height. I can add a couple other projects to fill up the space.
    
    There is currently a hardcoded button(link) to the founders pass demo site.
    
    * link logic
    
    the hyper link in the project card now only displays if there is a link sent to the project card. Otherwise it shows an empty container.
    
    * socials and const fixing
    
    I've changed the "contact me" section to "socials" which will only exist on smaller screens. On bigger screens I think I'm going to show them smallish on the side panel.

--- lib/screens/contact.dart
+++ lib/screens/contact.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:portfolio/widgets/section_title.dart';
+import 'package:portfolio/widgets/social_card.dart';
 
 class Contact extends ConsumerStatefulWidget {
   const Contact({super.key});
@@ -12,12 +13,21 @@ class Contact extends ConsumerStatefulWidget {
 class _ContactState extends ConsumerState<Contact> {
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height,
-      child: (const Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [SectionTitle(text: 'Contact Me')],
-      )),
-    );
+    return (Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const SectionTitle(text: 'Socials'),
+        GridView.count(
+          shrinkWrap: true,
+          padding: const EdgeInsets.all(20),
+          childAspectRatio: 1,
+          physics: const NeverScrollableScrollPhysics(),
+          crossAxisCount: 4,
+          children: const [
+            SocialCard(title: "GitHub", color: Colors.grey, destination: "https://github.com/chadlyalan", iconName: "github-mark-white.svg",)
+          ],)
+        ],
+    ));
   }
 }
--- lib/widgets/video_player_widget.dart
+++ lib/widgets/video_player_widget.dart
@@ -1,33 +1,50 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:portfolio/widgets/basic_overlay_widget.dart';
+import 'package:portfolio/widgets/secondaryButton.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:video_player/video_player.dart';
 
 class VideoPlayerWidget extends ConsumerWidget {
   final VideoPlayerController controller;
-  const VideoPlayerWidget({super.key, required this.controller});
+  final String? uri;
+  const VideoPlayerWidget({super.key, required this.controller, this.uri});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return controller.value.isInitialized
-        ? Container(
-            child: buildVideo(),
-          )
+        ? Column(
+          children: [
+           uri != null ?Padding(
+             padding: const EdgeInsets.only(bottom:8.0),
+             child: SecondaryButton(text: "a link",
+             callback: () async {
+    
+            final Uri url = Uri.parse('https://guides.rockgarden.io'); 
+             if (!await launchUrl(url)) {
+                throw Exception('could not luanch $url');
+              } 
+            },)) : Container(),
+            buildVideo(),
+          ],
+        )
         : const Center(
             child: CircularProgressIndicator(),
           );
   }
 
-  Widget buildVideo() => Stack(
-        children: [
-          buildVideoPlayer(),
-          Positioned.fill(
-            child: BasicOverlayWidget(
-              controller: controller,
-            ),
-          )
-        ],
-      );
+  Widget buildVideo() => Expanded(
+    child: Stack(
+          children: [
+            buildVideoPlayer(),
+            Positioned.fill(
+              child: BasicOverlayWidget(
+                controller: controller,
+              ),
+            )
+          ],
+        ),
+  );
 
   Widget buildVideoPlayer() => AspectRatio(
       aspectRatio: controller.value.aspectRatio,
